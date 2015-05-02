2f694e102bc24c3250f5676fdfc8147e577768ea
Andrew Golightly <andrew@golightlyplus.com>
2015-05-02 16:44:00 -0600

    improved easter egg fade in and out with no flicker

--- scripts/ag.js
+++ scripts/ag.js
@@ -20,6 +20,11 @@ agApp.directive('socialNetwork', function() {
 
 var egg = new Egg('babyandrew'.split('').toString(), function() {
 
-    $('.jumbotron img').attr("src","http://res.cloudinary.com/go-for-self/image/upload/c_scale,h_333/c_crop,h_333,w_333/v1430585072/Andrew%20Meditating.jpg").stop(true,true).hide().fadeIn();
+    $('.jumbotron img').fadeTo(2200, 0, function() {
+        setTimeout(function() {
+            $('.jumbotron img').attr("src","http://res.cloudinary.com/go-for-self/image/upload/c_scale,h_333/c_crop,h_333,w_333/v1430585072/Andrew%20Meditating.jpg");
+        },100);
+
+    }).delay(1100).fadeTo(1100, 1);
 
 }).listen();
