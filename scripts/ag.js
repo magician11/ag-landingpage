7382194bb8b5733819fafbe57e058683a7cf64cd
Andrew Golightly <andrew@golightlyplus.com>
2015-05-16 14:37:58 -0600

    animating only the profile pic now

--- scripts/ag.js
+++ scripts/ag.js
@@ -2,16 +2,23 @@ var agApp = angular.module('agApp', ['ngAnimate']);
 
 agApp.controller('AgCtrl', function($scope, $timeout) {
 
-    $timeout(function() {
-        $scope.agSocialNetworks = [
-            {icon: 'facebook', link: 'http://facebook.com/andrewgolightly11'}, 
-            {icon: 'twitter', link: 'http://twitter.com/AndrewGolightly'},
-            {icon: 'google-plus', link: 'https://plus.google.com/+AndrewGolightly/posts'},
-            {icon: 'github', link: 'https://github.com/magician11/'}
-        ];
-    }, 800);
-});
+    // set profile pic to be hidden on page load, then animate in
+    $scope.ag = {
+        showProfilePic: false
+    };
 
+    // create a slight delay as a workaround for animation to work on page load
+    $timeout(function() {
+        $scope.ag.showProfilePic = true;
+    },800);
+
+    $scope.agSocialNetworks = [
+        {icon: 'facebook', link: 'http://facebook.com/andrewgolightly11'}, 
+        {icon: 'twitter', link: 'http://twitter.com/AndrewGolightly'},
+        {icon: 'google-plus', link: 'https://plus.google.com/+AndrewGolightly/posts'},
+        {icon: 'github', link: 'https://github.com/magician11/'}
+    ];
+});
 
 agApp.directive('socialNetwork', function() {
 
@@ -19,14 +26,3 @@ agApp.directive('socialNetwork', function() {
         templateUrl: 'social-network.html'
     };
 });
-
-var egg = new Egg('babyandrew'.split('').toString(), function() {
-
-    $('.jumbotron img').fadeTo(2200, 0, function() {
-        setTimeout(function() {
-            $('.jumbotron img').attr("src","http://res.cloudinary.com/go-for-self/image/upload/c_scale,h_333/c_crop,h_333,w_333/v1430585072/Andrew%20Meditating.jpg");
-        },100);
-
-    }).delay(1100).fadeTo(1100, 1);
-
-}).listen();
