2b8a24d62fee11d164b0e59778c7a43eb785bac8
Andrew Golightly <andrew@golightlyplus.com>
2015-05-17 12:16:25 -0600

    added webserver and create profile pic container so there is a space on page load

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,32 @@
-/**
- * Plugins
- * -------
- */
-
+// get gulp and the plugins we need for it
 var gulp = require('gulp');
 var scss = require('gulp-sass');
 var bourbon = require('node-bourbon').includePaths;
+var webserver = require('gulp-webserver');
 
-/**
- * Tasks
- * -----
- */
-
-/**
- * Compiles SCSS files into CSS
- *
- * @source .scss files
- * @dest .css files
- */
+// setup our Sass compilation task
 gulp.task('scss', function() {
     return gulp.src('styles/ag.scss')
-    .pipe(scss({
+        .pipe(scss({
         errLogToConsole: true,
         includePaths: ['styles'].concat(bourbon)
     }))
-    .pipe(gulp.dest('dist/css'));
+        .pipe(gulp.dest('dist/css'));
 });
 
+// setup our webserver
+gulp.task('webserver', function() {
+    gulp.src('.')
+        .pipe(webserver({
+        livereload: true,
+        open: true
+    }));
+});
 
-/**
- * Watch for any scss changes
- */
-
-gulp.task('default', ['scss'], function() {
+// watch our scss files for changes
+gulp.task('watch', function() {
     gulp.watch('styles/**/*.scss', ['scss']);
 });
+
+// run our tasks on running 'gulp' from the command line
+gulp.task('default', ['webserver', 'scss', 'watch']);
--- scripts/ag.js
+++ scripts/ag.js
@@ -2,17 +2,14 @@ var agApp = angular.module('agApp', ['ngAnimate']);
 
 agApp.controller('AgCtrl', function($scope, $timeout) {
 
-    // set profile pic to be hidden on page load, then animate in
-    $scope.ag = {
-        showProfilePic: false
-    };
+    $scope.ag = {};
 
     // create a slight delay as a workaround for animation to work on page load
     $timeout(function() {
-        $scope.ag.showProfilePic = true;
+        $scope.ag.profilePic = 'http://res.cloudinary.com/go-for-self/image/upload/Andrew-Golightly-profile.jpg';
     },800);
 
-    $scope.agSocialNetworks = [
+    $scope.ag.socialNetworks = [
         {icon: 'facebook', link: 'http://facebook.com/andrewgolightly11'}, 
         {icon: 'twitter', link: 'http://twitter.com/AndrewGolightly'},
         {icon: 'google-plus', link: 'https://plus.google.com/+AndrewGolightly/posts'},
