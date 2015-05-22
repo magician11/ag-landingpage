7d5aea6eca98d6f6540461ed56d21a3738e7b3ca
Andrew Golightly <andrew@golightlyplus.com>
2015-05-21 22:26:26 -0600

    refactored structure as per AngularJS best practices... and updated gulpfile.js accordingly

--- gulpfile.js
+++ gulpfile.js
@@ -5,19 +5,36 @@ var bourbon = require('node-bourbon').includePaths;
 var webserver = require('gulp-webserver');
 var jshint = require('gulp-jshint');
 
+// set our paths
+var src = 'app';
+var dist = 'dist';
+var paths = {
+    js: src + '/*.js',
+    scss: src + '/*.scss',
+    html: src + '/*.html'
+};
+
 // setup our Sass compilation task
 gulp.task('scss', function() {
-    return gulp.src('styles/ag.scss')
+    return gulp.src(paths.scss)
         .pipe(scss({
         errLogToConsole: true,
         includePaths: ['styles'].concat(bourbon)
     }))
-        .pipe(gulp.dest('dist/css'));
+        .pipe(gulp.dest(dist + '/css'));
+});
+
+// copy across our html files
+gulp.task('html', function() {
+   
+    return gulp.src(paths.html)
+    .pipe(gulp.dest(dist));
+    
 });
 
 // setup our webserver
 gulp.task('webserver', function() {
-    gulp.src('.')
+    gulp.src(dist)
         .pipe(webserver({
         livereload: true,
         open: true
@@ -25,17 +42,19 @@ gulp.task('webserver', function() {
 });
 
 // quality check our JS
-gulp.task('lint', function() {
-    return gulp.src('scripts/*.js')
+gulp.task('scripts', function() {
+    return gulp.src(paths.js)
     .pipe(jshint())
-    .pipe(jshint.reporter('default'));
+    .pipe(jshint.reporter('default'))
+    .pipe(gulp.dest(dist));
 });
 
-// watch our scss files for changes
+// watch our files for changes
 gulp.task('watch', function() {
-    gulp.watch('styles/**/*.scss', ['scss']);
-    gulp.watch('scripts/*.js', ['lint']);
+    gulp.watch(paths.scss, ['scss']);
+    gulp.watch(paths.js, ['scripts']);
+    gulp.watch(paths.html, ['html']);
 });
 
 // run our tasks on running 'gulp' from the command line
-gulp.task('default', ['webserver', 'watch']);
+gulp.task('default', ['webserver', 'scss', 'scripts', 'html', 'watch']);
