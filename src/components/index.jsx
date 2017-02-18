ee7f4870d537454ea8d2d863bda9adcb7e318be2
Andrew Golightly <support@andrewgolightly.com>
2017-02-18 18:50:28 +0700

    improved styling and added copyright

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -26,19 +26,19 @@ export default class App extends Component {
         <div className="text-center">
           <Image src="http://res.cloudinary.com/go-for-self/image/upload/c_scale,w_800/andrewgolightly.com/Andrew_in_Yangon.jpg" thumbnail title="Andrew Golightly" />
           <h1>Andrew Golightly</h1>
-          <h2>web developer & counsellor</h2>
+          <h3>web developer & counsellor</h3>
           <br/ >
             <a className="btn btn-default btn-success btn-lg" href="https://docs.google.com/forms/d/e/1FAIpQLSdNiATjYoGgQJcR-DBYoOZcxsaGkNyz0C2pb2UTCNg8AulIuA/viewform" role="button">Click Here To Hire Me</a>
             <hr />
-            <Row>
+            <Row className="footer">
               <Col xs={12} sm={6}>
-                Designed and built by Andrew Golightly
+                <i className="fa fa-copyright" aria-hidden="true"></i> { new Date().getFullYear() } Created by Andrew Golightly
               </Col>
               <Col xs={12} sm={6}>
                 <ul className="list-inline">
-                  <li><a href="https://twitter.com/AndrewGolightly"><i className="fa fa-twitter fa-2x" aria-hidden="true"></i></a></li>
-                  <li><a href="https://www.facebook.com/andrewgolightly11"><i className="fa fa-facebook fa-2x" aria-hidden="true"></i></a></li>
-                  <li><a href="https://github.com/magician11/"><i className="fa fa-github fa-2x" aria-hidden="true"></i></a></li>
+                  <li><a href="https://twitter.com/AndrewGolightly" title="Andrew Golightly on Twitter"><i className="fa fa-twitter fa-2x" aria-hidden="true"></i></a></li>
+                  <li><a href="https://www.facebook.com/andrewgolightly11" title="Andrew Golightly on Facebook"><i className="fa fa-facebook fa-2x" aria-hidden="true"></i></a></li>
+                  <li><a href="https://github.com/magician11/" title="Andrew Golightly (magician11) on GitHub"><i className="fa fa-github fa-2x" aria-hidden="true"></i></a></li>
                 </ul>
               </Col>
             </Row>
